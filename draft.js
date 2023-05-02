a8d175246c7d7698f587eccadf07b637d90a5e71
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-03 09:16:11 +1000

    Add files via upload

--- draft.js
+++ draft.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const app = express();
+// open a connection to the database
+const request = indexedDB.open('flashcard_db', 1);
 
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'myproject';
-const collectionName = 'nodes';
+// handle errors when opening the database
+request.onerror = function(event) {
+  console.log('Error opening database:', event.target.errorCode);
+};
 
-app.get('/node/:id', function(req, res) {
-    const nodeId = req.params.id;
+// create the object store to store flashcard sets
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  const objectStore = db.createObjectStore('flashcard_sets', { keyPath: 'id' });
+  objectStore.createIndex('name', 'name', { unique: true });
+};
 
-    MongoClient.connect(url, function(err, client) {
-        if (err) {
-            console.log(err);
-            return;
-        }
+// add a new flashcard set to the database
+function addFlashcardSet(flashcardSet) {
+  const transaction = db.transaction(['flashcard_sets'], 'readwrite');
+  const objectStore = transaction.objectStore('flashcard_sets');
+  const request = objectStore.add(flashcardSet);
+  request.onsuccess = function(event) {
+    console.log('Flashcard set added to database');
+  };
+  request.onerror = function(event) {
+    console.log('Error adding flashcard set to database:', event.target.errorCode);
+  };
+}
 
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-
-        collection.findOne({ id: nodeId }, function(err, node) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            res.send(node);
-            client.close();
-        });
-    });
-});
-
-app.listen(3000, function() {
-    console.log('App listening on port 3000!');
-});
+// get a flashcard set from the database
+function getFlashcardSet(id, callback) {
+  const transaction = db.transaction(['flashcard_sets'], 'readonly');
+  const objectStore = transaction.objectStore('flashcard_sets');
+  const request = objectStore.get(id);
+  request.onsuccess = function(event) {
+    const flashcardSet = event.target.result;
+    callback(flashcardSet);
+  };
+  request.onerror = function(event) {
+    console.log('Error getting flashcard set from database:', event.target.errorCode);
+  };
+}
